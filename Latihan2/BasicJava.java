f3a58b0fbea2f30fa069148f772c896f6ff0da9e
M-BintangR <muhbintang650@gmail.com>
2025-03-17 09:28:50 +0800

    [TUTOR] input data lewat cmd / console

--- Latihan2/BasicJava.java
+++ Latihan2/BasicJava.java
@@ -1,4 +1,6 @@
 package Latihan2;
+import java.util.Scanner;
+
 public class BasicJava {
     public static void tampil(){
         System.out.println("Muhammad Bintang");
@@ -17,5 +19,23 @@ public class BasicJava {
         System.out.println("Tinggi Badan : " + tinggi);
         System.out.println("Status Mahasiswa : " + status);
         System.out.println("Grade : " + NA);
+    }
+
+    public static void var_input(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Masukkan nama anda : ");
+        String nama = scanner.nextLine();
+        
+        System.out.println("Masukkan nim anda : ");
+        String nim = scanner.nextLine();
+
+        System.out.println("Masukkan status anda : ");
+        String status = scanner.nextLine();
+
+        System.out.println("----------------Data Anda------------------");
+        System.out.println("Nama   : " + nama);
+        System.out.println("NIM    : " + nim);
+        System.out.println("Status : " + status);
     }
 }
--- Latihan2/ParaktikumDasarJava.java
+++ Latihan2/ParaktikumDasarJava.java
@@ -1,11 +1,13 @@
 package Latihan2;
 public class ParaktikumDasarJava {
     public static void main(String[] args) {
-        System.out.println("Isi Method Tampil");
-        System.out.println("--------------------------------------------");
-        BasicJava.tampil();
-        System.out.println("Isi Method Varibel");
-        System.out.println("--------------------------------------------");
-        BasicJava.variable();
+        // System.out.println("Isi Method Tampil");
+        // System.out.println("--------------------------------------------");
+        // BasicJava.tampil();
+        // System.out.println("Isi Method Varibel");
+        // System.out.println("--------------------------------------------");
+        // BasicJava.variable();
+
+        BasicJava.var_input();
     }
 }
